0751ec47f56be1838151e50a3f10f7bb4dc34426
pshukla25 <pshukla25@yahoo.com>
2017-10-23 15:29:46 -0400

    testing

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ var express = require('express');
 
 var app = express();
 
-var reload = require('reload')
-
 app.get('/', function (req, res) {
   res.send('Prestoo Magico!!!');
 });
 
-reload(app);
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
